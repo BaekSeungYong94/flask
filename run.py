a34302c539ab6349593fecc3a2ab0c891e8114c2
BaekSeungYong94 <sybaek17@gmail.com>
2024-04-21 11:02:27 +0900

    flask route 및 구조 테스트

--- run.py
+++ run.py
@@ -3,8 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return 'hello world'
+    return 'hello home'
+
+@app.route('/user/<user_name>/<int:user_id>')
+def user(user_name,user_id):
+    #return 'hello user'
+    return f'Hello, {user_name}({user_id})!'
 
 if __name__ == "__main__":
     app.run(debug=True)
